738867a16b3925618669673d5e5e6e6194842571
Jeziel Francisco da Silva <jeziel.franciscodasilva@gmail.com>
2018-05-01 23:11:57 -0300

    Inicial

--- build/models/index.js
+++ build/models/index.js
@@ -17,7 +17,7 @@ else {
 fs
     .readdirSync(__dirname)
     .filter(function (file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.ts');
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
 })
     .forEach(function (file) {
     var model = sequelize['import'](path.join(__dirname, file));

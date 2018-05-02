9710a4b3679f1b15f23b9ed28f71c958c582d31e
Jeziel Francisco da Silva <jeziel.franciscodasilva@gmail.com>
2018-05-01 22:50:18 -0300

    Inicial

--- build/config/config.js
+++ build/config/config.js
@@ -3,13 +3,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var CONFIG = {
     env: 'development',
     serverPort: 3000,
-    host: '',
-    username: '',
-    password: '',
-    db: '',
-    dialect: '',
+    host: 'mysql.api-sale.kinghost.net',
+    username: 'apisale',
+    password: '010065363je',
+    db: 'apisale',
+    dialect: 'mysql',
     secret: 'KSAJASJDhaoi418KDASHDAJH1098319080dajkldjaou102a21fads1ad51hA)(*@#)$',
-    dbPort: 0,
-    Url: 'dialect://user:password@host:port/database'
+    dbPort: 3306,
+    Url: 'mysql://apisale:010065363je@mysql.api-sale.kinghost.net:3306/apisale'
 };
 exports.default = CONFIG;
